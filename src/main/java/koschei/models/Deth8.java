459f4cdb19b6211086f51addcf8355870a46b8f4
Nikita Pastukhov <pastukhovns@yandex.ru>
2024-06-08 15:40:38 +0500

    Completed

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,17 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+//    private Deth8 deth;
+//
+//    public Deth8(Deth8 deth) {
+//        this.deth = deth;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "!!!";
+//    }
+}
